918c56ec4c3221a75b4eaec7a502144c67c42aaa
edoardominelli04-droid <edoardominelli04@gmail.com>
2026-07-01 01:15:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
+
+print("ENV KEYS:", list(os.environ.keys()))
+print("DISCORD_TOKEN RAW:", os.environ.get("DISCORD_TOKEN"))
+
+token = os.environ.get("DISCORD_TOKEN")
+
+if not token:
+    raise Exception("DISCORD_TOKEN NON VISTA DA RAILWAY")
+
 import discord
 from discord.ext import commands
-
-TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot online come {bot.user}")
+    print("BOT ONLINE:", bot.user)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong 🟢")
-
-bot.run(TOKEN)
+bot.run(token)
